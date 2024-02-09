d097d932cbe9afb14010de4672196c586e59f508
jenzie68 <jacecabalo@gmail.com>
2024-02-09 09:38:06 +0400

    initial file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { hashmap } from "./hashmap";
+
+let test = hashmap();
+test.set('a','aoll');
+test.set('b','boll');
+test.set('c','coll');
+test.set('d','doll');
+test.set('e','eoll');
+test.set('f','foll');
+test.set('g','goll');
+test.set('h','holl');
+test.set('i','uoll');
+test.set('j','joll');
+test.set('k','koll');
+test.set('l','loll');
+test.set('m','aoll');
