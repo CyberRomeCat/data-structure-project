92777a271ba8aea16d179bc0c4279b692ceacceb
jenzie68 <jacecabalo@gmail.com>
2024-02-09 09:41:08 +0400

    export hashmap

--- src/hashmap.js
+++ src/hashmap.js
@@ -191,19 +191,4 @@ function hashmap() {
     return {set, get, has, remove, length, clear, keys, values, entries, allBuckets}
 }
 
-let test = hashmap();
-test.set('a','aoll');
-test.set('b','boll');
-test.set('c','coll');
-test.set('d','doll');
-test.set('e','eoll');
-test.set('f','foll');
-test.set('g','goll');
-test.set('h','holl');
-test.set('i','uoll');
-test.set('j','joll');
-test.set('k','koll');
-test.set('l','loll');
-test.set('m','aoll');
-
 export { hashmap }
